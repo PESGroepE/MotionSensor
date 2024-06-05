b3ef79c83ee96d4c5f711b952f73eb3f348226af
ItIsJQ <julianrijkers@gmail.com>
2024-06-05 12:48:18 +0200

    disabled and removed motionsensor

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,61 +1,33 @@
-#include "MotionSensor.h"
 #include <ESP8266HTTPClient.h>
-// #include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
 #include "RGBLeds.h"
 
+const char* ssid = "pigroep5";
+const char* password = "pigroep5";
+const char* serverAddress = "10.0.10.1";
+const int serverPort = 8080;
+const String host = "10.0.10.1:8080";
 
-// Define the number of LEDs in the strip
-// #define NUM_LEDS 60
-// #define LED_PIN D6
-
-// Create an instance of the Adafruit_NeoPixel class
-// Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-RGBLeds strip;
-
-// WiFi settings
-const char* ssid = "pigroep5"; // Vervang door je WiFi SSID
-const char* password = "pigroep5"; // Vervang door je WiFi wachtwoord
-
-// Server settings
-const char* serverAddress = "10.0.10.1"; // Vervang door het IP-adres of de hostname van je server
-const int serverPort = 8080; // Poortnummer van de server
-
-String host = "10.0.10.1:8080";  // Vervang door het IP-adres of hostname van je Raspberry Pi
-
-bool previousStatus = false;
-
-MotionSensor motionSensor(serverAddress, serverPort);
 WiFiClient client;
 HTTPClient http;
+RGBLeds strip;
 
 void setup() {
-    Serial.begin(115200);
-
-    // Initialize the NeoPixel strip
-    // strip.begin();
-    // strip.setBrightness(25);
-    // strip.show(); // Initialize all pixels to 'off'
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("Connected to WiFi");
-    Serial.println(WiFi.localIP());
-
-    motionSensor.initialisatie();
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    //motionSensor.startDetectie(client);
-    //delay(100);
-    if (getBrand()) return;
-    getColor();
-    getStatus();
-    delay(1000);
-  
+  if (getBrand()) return;
+  getColor();
+  getStatus();
+  delay(1000);
 }
 
 /**
@@ -63,32 +35,29 @@ void loop() {
   */
 void getStatus() {
   // Get request
-    String url = "http://" + host + "/led/status";  // Zorg dat dit het juiste endpoint is
-
-    if (http.begin(client, url)) {  // HTTP
-        Serial.print("[HTTP] GET...\n");
-        int httpCode = http.GET();
-
-        // httpCode zal negatief zijn bij een fout
-        if (httpCode < 0) {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-            if (payload == "1") {    
-                // aan
-                strip.entreelichtAan();
-            } else {
-                // uit
-                strip.lichtUit();
-            }
-        }
-        http.end();
-    } else {
-        Serial.println("[HTTP] Unable to connect");
+  String url = "http://" + host + "/led/status";  // Zorg dat dit het juiste endpoint is
+  if (http.begin(client, url)) {                  // HTTP
+    Serial.print("[HTTP] GET...\n");
+    int httpCode = http.GET();
+    // httpCode zal negatief zijn bij een fout
+    if (httpCode < 0) {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      String payload = http.getString();
+      Serial.println(payload);
+      if (payload == "1") {
+        // aan
+        strip.entreelichtAan();
+      } else {
+        // uit
+        strip.lichtUit();
+      }
     }
-
-    delay(100);
+    http.end();
+  } else {
+    Serial.println("[HTTP] Unable to connect");
+  }
+  delay(100);
 }
 
 /**
@@ -96,34 +65,33 @@ void getStatus() {
   */
 void getColor() {
   // Get request
-    String url = "http://" + host + "/led/color";  // Zorg dat dit het juiste endpoint is
+  String url = "http://" + host + "/led/color";  // Zorg dat dit het juiste endpoint is
 
-    if (http.begin(client, url)) {  // HTTP
-        Serial.print("[HTTP] GET...\n");
-        int httpCode = http.GET();
+  if (http.begin(client, url)) {  // HTTP
+    Serial.print("[HTTP] GET...\n");
+    int httpCode = http.GET();
 
-        // httpCode zal negatief zijn bij een fout
-        if (httpCode < 0) {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-            if (payload == "rood") {    
-                strip.setKleur(255, 0, 0);
-            } else if (payload == "groen") {  
-                strip.setKleur(0, 255, 0);
-            } else if (payload == "blauw") {  
-                strip.setKleur(0, 0, 255);
-            } else if (payload == "wit") {
-                strip.setKleur(255,255,255);
-            }
-        }
-        http.end();
-    } else {
-        Serial.println("[HTTP] Unable to connect");
+    // httpCode zal negatief zijn bij een fout
+    if (httpCode < 0) {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      String payload = http.getString();
+      Serial.println(payload);
+      if (payload == "rood") {
+        strip.setKleur(255, 0, 0);
+      } else if (payload == "groen") {
+        strip.setKleur(0, 255, 0);
+      } else if (payload == "blauw") {
+        strip.setKleur(0, 0, 255);
+      } else if (payload == "wit") {
+        strip.setKleur(255, 255, 255);
+      }
     }
-
-    delay(100);
+    http.end();
+  } else {
+    Serial.println("[HTTP] Unable to connect");
+  }
+  delay(100);
 }
 
 /**
@@ -131,30 +99,29 @@ void getColor() {
   */
 bool getBrand() {
   // Get request
-    String url = "http://" + host + "/led/brand";  // Zorg dat dit het juiste endpoint is
+  String url = "http://" + host + "/led/brand";  // Zorg dat dit het juiste endpoint is
 
-    if (http.begin(client, url)) {  // HTTP
-        Serial.print("[HTTP] GET...\n");
-        int httpCode = http.GET();
+  if (http.begin(client, url)) {  // HTTP
+    Serial.print("[HTTP] GET...\n");
+    int httpCode = http.GET();
 
-        // httpCode zal negatief zijn bij een fout
-        if (httpCode < 0) {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-            if (payload == "1") {    
-                strip.brandlichtAan();
-                return true;
-            } else {
-                return false;
-            }
-        }
-        http.end();
-    } else {
-        Serial.println("[HTTP] Unable to connect");
+    // httpCode zal negatief zijn bij een fout
+    if (httpCode < 0) {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    } else if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      String payload = http.getString();
+      Serial.println(payload);
+      if (payload == "1") {
+        strip.brandlichtAan();
+        return true;
+      } else {
+        return false;
+      }
     }
-
-    delay(100);
-    return false;
+    http.end();
+  } else {
+    Serial.println("[HTTP] Unable to connect");
+  }
+  delay(100);
+  return false;
 }
